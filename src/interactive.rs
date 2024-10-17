2496ef6a2fd87fe4203ec86c4423ccbdf151af9c
GAO <gaoxiangshuai@gmail.com>
2024-10-16 21:40:08 -0400

    updated

--- src/interactive.rs
+++ src/interactive.rs
@@ -24,9 +24,57 @@ fn print_menu(){
     );
 }
 
+fn print_add_fund(){
+    print!(
+    r###"
+        Please add your funds below, use negative number to subtract, if you wish to cancel, simply enter zero
+    "###
+    )
+}
 
-pub fn interactive_menu(){
-    print_menu();
+fn print_current_fund(){
+    print!(
+    r###"
+        You have ${}
+    "###, 123);
+}
+
+fn add_fund(amount: f32){
+
+}
+
+fn interactive_add_fund(){
+    print_current_fund();
+    print_add_fund();
+    
+    loop {
+       
+        let mut text = String::new();
+        io::stdin()
+            .read_line(&mut text)
+            .expect("failed to read from stdin");
+
+        let amount: Result<f32, _> = text.trim().parse();
+        if let Err(_) = amount{
+            print!(
+            r###"
+                Please enter a valid amount
+            "###
+            );
+            continue;
+        }
+
+        add_fund(amount.unwrap());
+        println!(
+            r###"
+                Fund added
+            "###
+        );
+        break;
+    }
+}
+
+pub fn ask_menu_option() -> i32{
     let mut input_text = String::new();
     io::stdin()
         .read_line(&mut input_text)
@@ -35,15 +83,23 @@ pub fn interactive_menu(){
         Ok(e) => e,
         Err(_) => -1
     };
+    user_option
+}
+
+pub fn interactive_menu(){
+    print_menu();
+    let mut user_option = ask_menu_option();
 
     loop{
         match user_option {
-            user_option if user_option == MenuOption::AddFund as i32 => println!("To be implemented"),
+            user_option if user_option == MenuOption::AddFund as i32 => interactive_add_fund(),
             user_option if user_option == MenuOption::CreatePie as i32 => println!("To be implemented"),
             user_option if user_option == MenuOption::ModifyPie as i32 => println!("To be implemented"),
             user_option if user_option == MenuOption::Exit as i32 => break,
             _ => println!("Please select a valid option")
         } 
+        print_menu();
+        user_option = ask_menu_option();
     }
     
     
